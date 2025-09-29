27800bad70eea5eb28a1760ea71e9b701632edff
CAPEL Bastien <capel.direction@gmail.com>
2025-09-29 20:39:47 +0000

    Rebrand site as examen.blanc.LFJP

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="space-y-2">
       <h1 className="text-3xl font-bold text-slate-900">
-        Tableau de Bord - Baccalauréat Blanc
+        Tableau de Bord - examen.blanc.LFJP
       </h1>
       <p className="text-lg text-slate-600">10, 11 et 12 Décembre 2025</p>
     </header>
