d22f2ce44c7a2fe4b6e68d2a12104c0ab538294d
CAPEL Bastien <capel.direction@gmail.com>
2026-01-07 13:36:47 +0100

    Update DNB blanc subject deadline

--- src/features/exam-dashboard/components/Footer.tsx
+++ src/features/exam-dashboard/components/Footer.tsx
@@ -3,9 +3,9 @@ export default function Footer() {
     <footer className="rounded-lg bg-blue-100 p-4 text-center text-blue-800">
       <p className="font-semibold">Rappel :</p>
       <p>
-        Envoi des sujets au plus tard le 1er décembre 2025, comme indiqué dans
-        l'en-tête. Présence des surveillants 20 min avant le début de
-        l'épreuve.
+        Pour le DNB blanc des 3 et 4 février, dépôt des sujets au plus tard le
+        vendredi 23 janvier 2026. Présence des surveillants 20 min avant le
+        début de l&apos;épreuve.
       </p>
     </footer>
   );
